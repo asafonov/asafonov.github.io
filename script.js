eae21fd51662a29ee42c2f04dd363705dea52a39
asafonov <me@asafonov.org>
2023-03-06 16:37:45 +0100

    minor fix for script

--- script.js
+++ script.js
@@ -1,40 +1,42 @@
 function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-  document.querySelector('.home').classList.add('glitch');
+  document.body.scrollTop = 0
+  document.documentElement.scrollTop = 0
+  document.querySelector('.home').classList.add('glitch')
 }
 
 function menuFunction (el) {
-  const x = document.querySelector(".top_menu_mobile");
+  const x = document.querySelector(".top_menu_mobile")
 
   if (x === null || x === undefined) {
-    return ;
+    return
   }
 
-  const isFlex = x.style.display == 'flex';
-  x.style.display = isFlex ? 'none' : 'flex';
-  el.setAttribute('data-before', isFlex ? '///' : "X");
+  const isFlex = x.style.display === 'flex'
+  x.style.display = isFlex ? 'none' : 'flex'
+  el.setAttribute('data-before', isFlex ? '///' : "X")
 }
 
 function leftmenuFunction (el, event) {
-  event.preventDefault();
-  el.parentNode.classList.add('active');
-  document.querySelector('.left_menu').classList.add('sub');
-  const href = el.getAttribute('href');
-  const goto = function() {location.href = href;};
-  setTimeout(goto, 1300);
+  event.preventDefault()
+  el.parentNode.classList.add('active')
+  document.querySelector('.left_menu').classList.add('sub')
+  const href = el.getAttribute('href')
+  setTimeout(() => {
+    el.parentNode.classList.remove('active')
+    location.href = href
+  }, 1300)
 }
 
 function handleTiramisuVisibility() {
-  document.querySelector('.tiramisu').style.display = document.body.offsetHeight < document.body.scrollHeight ? 'block' : 'none';
+  document.querySelector('.tiramisu').style.display = document.body.offsetHeight < document.body.scrollHeight ? 'block' : 'none'
 }
 
 function onResize() {
-  document.querySelector('.tiramisu').style.display = 'none';
-  handleTiramisuVisibility();
+  document.querySelector('.tiramisu').style.display = 'none'
+  handleTiramisuVisibility()
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  window.addEventListener('resize', onResize);
-  window.addEventListener('scroll', handleTiramisuVisibility);
-});
+  window.addEventListener('resize', onResize)
+  window.addEventListener('scroll', handleTiramisuVisibility)
+})
